f63829ea97e0d942fc45d0b9cbd8a77f0e7cced8
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-04-26 15:14:46 +0900

    ✏️ Props exam with Arrays

--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,31 @@
 import React, {Component} from 'react';
 
 class Table extends Component {
-render() {
+  render() {
+    const {characterData} = this.props
+
     return (
-    <table>
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>Job</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-        </tr>
-        <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-        </tr>
-        <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-        </tr>
-        <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-        </tr>
-        </tbody>
-    </table>
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData}/>
+      </table>
     )
-    }
+  }
 }
 
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+
+  const TableBody = () => {
+    return <tbody />
+  }
+  
 export default Table
