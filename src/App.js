af792c35a32afa141f5782ffa2fafe934a94607c
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-04-26 15:51:08 +0900

    ✏️ Props exam with State

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Table from './Table';
 
 class App extends Component{
-  render() {
-    const characters = [
+  state = {
+    characters : [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -22,10 +22,24 @@ class App extends Component{
         job: 'Bartender',
       },
     ]
+  }
 
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+  
     return (
       <div className="container">
-        <Table characterData={characters} />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 
-class Table extends Component {
-  render() {
-    const {characterData} = this.props
+const Table = (props) => {
+  const {characterData, removeCharacter} = props
 
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={characterData}/>
-      </table>
-    )
-  }
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
 
 const TableHeader = () => {
@@ -19,13 +17,26 @@ const TableHeader = () => {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     )
   }
 
-  const TableBody = () => {
-    return <tbody />
-  }
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
+}
   
 export default Table
