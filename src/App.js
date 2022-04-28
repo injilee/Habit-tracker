2345fba055277159e0b449f803ac61b9e33c2e71
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-04-28 13:40:33 +0900

    ✏️ Form update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component{
   state = {
-    characters : [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters : []
+  }
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
   }
 
   removeCharacter = (index) => {
@@ -40,6 +28,7 @@ class App extends Component{
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
