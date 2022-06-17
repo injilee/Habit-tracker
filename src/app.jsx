d3802af31fd144c296793a1997e8c623a26e02fe
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-06-17 23:16:18 +0900

    각 모듈별 컴포넌트 재배치 및 PureComponent화

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,78 @@
 import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
+import NavBar from './components/navBar';
 
 export default class App extends Component{
-    state = {
-      totalCount : 0
+      state = {
+        habits : [
+            { id:1, name: 'Reading', count: 0},
+            { id:2, name: 'Running', count: 0},
+            { id:3, name: 'Coding', count: 0}
+        ]
     }
 
-    // totalCount = habit => {
-    //   this.setState(habit);
-    // }
+    handleIncrement = habit => {
+        const habits = this.state.habits.map(item => {
+          if(item.id === habit.id){
+            return { ...habit, count : habit.count + 1};
+          }
+          return item;
+        })
+        this.setState({ habits });
+        // console.log(this.state.habits);
+    }
+
+    handleDecrement = habit => {
+        const habits = this.state.habits.map(item => {
+          if(item.id === habit.id){
+            const count = habit.count - 1;
+            return { ...habit, count : count < 0 ? 0 : count};
+          }
+          return item;
+        })
+        this.setState({ habits });
+    }
+
+    handleDelete = habit => {
+        // splice
+        // const habits = [...this.state.habits];
+        // const index = habits.indexOf(habit);
+        // habits.splice(index, 1);
+
+        // filter
+        const habits = this.state.habits.filter((item) => item.id !== habit.id);
+        this.setState({ habits });
+    }
+
+    handleAdd = habitName => {
+        const habits = [...this.state.habits,{id: Date.now(), name: habitName, count: 0} ];
+        this.setState({ habits });
+    }
+
+    handleReset = () => {
+        const habits = this.state.habits.map((habit) => {
+            habit.count = 0
+            return habit;
+        });
+        this.setState({ habits });
+        console.log(habits);
+    }
 
     
     render(){
       return (
-        <React.StrictMode>
-          <Habits />
-        </React.StrictMode>
+          <>
+            <NavBar totalCount={this.state.habits.filter(item => item.count > 0).length} />
+            <Habits  
+              habits={this.state.habits}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+              onAdd={this.handleAdd}
+              onReset={this.handleReset}
+            />
+          </>
         )
     }
 }
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,77 +1,43 @@
 import React, { Component } from 'react'
 import Habit from './habit'
 import HabitAddForm from './habitAddForm'
-import NavBar from './navBar'
 
-export default class Habits extends Component {
-    state = {
-        habits : [
-            { id:1, name: 'Reading', count: 0},
-            { id:2, name: 'Running', count: 0},
-            { id:3, name: 'Coding', count: 0}
-        ]
-    }
-
+class Habits extends Component {
     handleIncrement = habit => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
-        this.setState({ habits });
-        // console.log(this.state.habits);
+        this.props.onIncrement(habit);
     }
 
     handleDecrement = habit => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count - 1;
-        habits[index].count = count < 0 ? 0 : count;
-        this.setState({ habits });
+        this.props.onDecrement(habit);
     }
 
     handleDelete = habit => {
-        // splice
-        // const habits = [...this.state.habits];
-        // const index = habits.indexOf(habit);
-        // habits.splice(index, 1);
-
-        // filter
-        const habits = this.state.habits.filter((item) => item.id !== habit.id);
-        this.setState({ habits });
+        this.props.onDelete(habit);
     }
-
-    handleAdd = habitName => {
-        const habits = [...this.state.habits,{id: Date.now(), name: habitName, count: 0} ];
-        this.setState({ habits });
-    }
-
-    handleReset = () => {
-        const habits = this.state.habits.map((habit) => {
-            habit.count = 0
-            return habit;
-        });
-        this.setState({ habits });
-        console.log(habits);
+    
+    handleAdd = name => {
+        this.props.onAdd(name);
     }
 
     render() {
         return (
             <div className='habits'>
-                <NavBar totalCount={this.state.habits.filter(item => item.count > 0).length} />
-                <HabitAddForm onAdd={this.handleAdd}/>
-
+                <HabitAddForm onAdd={this.handleAdd} />
                 <ul>
-                    {this.state.habits.map(habits => (
+                    {this.props.habits.map(habits => (
                     <Habit 
-                    key={habits.id}
-                    habits={habits}
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                    onDelete={this.handleDelete}
+                        key={habits.id}
+                        habits={habits}
+                        onIncrement={this.handleIncrement}
+                        onDecrement={this.handleDecrement}
+                        onDelete={this.handleDelete}
                     />
                 ))}
                 </ul>
-                <button className='reset-btn' onClick={this.handleReset}>Reset All</button>
+                <button className='reset-btn' onClick={this.props.onReset}>Reset All</button>
             </div>
         )
     }
 }
+
+export default Habits;
